56b69b6460f8557962d4edd7510605ad56898fed
Kdasaradha 525 <kdasaradha525@gmail.com>
2025-06-25 15:13:30 +0000

    src/components/auth/AuthTypeSetups.tsx (18:6). Can you stop using this f

---
+++ src/components/auth/PlaceholderSetup.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function PlaceholderSetup() {
+  return (
+    <div className="text-muted-foreground">
+      <p>Setup instructions are currently being revised and will be available soon.</p>
+    </div>
+  );
+}
